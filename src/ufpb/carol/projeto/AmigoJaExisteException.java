37054dc13f09732ec3c5cdccbe12205a976ad10d
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:41:44 -0300

    Create AmigoJaExisteException.java

---
+++ src/ufpb/carol/projeto/AmigoJaExisteException.java
@@ -0,0 +1,10 @@
+package ufpb.carol.projeto;
+
+public class AmigoJaExisteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public AmigoJaExisteException(String msg){
+        super(msg);
+    }
+
+}
