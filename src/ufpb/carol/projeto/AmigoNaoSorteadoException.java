694cad6ffbf31f8bdfbaa6ba05eae02c730fad1f
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:42:33 -0300

    Create AmigoNaoSorteadoException.java

---
+++ src/ufpb/carol/projeto/AmigoNaoSorteadoException.java
@@ -0,0 +1,10 @@
+package ufpb.carol.projeto;
+
+public class AmigoNaoSorteadoException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public AmigoNaoSorteadoException(String msg){
+        super(msg);
+    }
+    
+}
